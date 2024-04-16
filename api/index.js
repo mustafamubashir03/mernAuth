ccc86eaf02875bc676005d4cd9d57a6873359564
mustafamubashir03 <114705378+mustafamubashir03@users.noreply.github.com>
2024-04-16 11:31:32 +0500

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,12 +1,18 @@
-import express from 'express';
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
 const app = express();
-
+dotenv.config();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("connected to Mongodb"))
+  .catch((err) => console.log(err));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(3000, () => {
-  console.log('App has been started');
+  console.log("App has been started");
 });
