f9e7d0445a8fcbe528b97cea31adfadfdd97e3da
mustafamubashir03 <114705378+mustafamubashir03@users.noreply.github.com>
2024-04-17 08:40:22 +0500

    Controller routes added

--- api/index.js
+++ api/index.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
 import express from "express";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.router.js";
 const app = express();
 dotenv.config();
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO)
-  .then(() => console.log("connected to Mongodb"))
-  .catch((err) => console.log(err));
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+.connect(process.env.MONGO)
+.then(() => console.log("connected to Mongodb"))
+.catch((err) => console.log(err));
+
+app.use("/api/user", userRoute); 
 
 app.listen(3000, () => {
   console.log("App has been started");
